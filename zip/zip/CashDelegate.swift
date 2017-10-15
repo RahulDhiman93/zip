1f649a1d106da6fc260af5a6c611345e5123a9e9
Rahul Dhiman <rahuldhiman@Rahuls-MacBook-Pro.local>
2017-10-15 15:40:52 +0530

    Adding cashDelegate

--- zip/zip/CashDelegate.swift
+++ zip/zip/CashDelegate.swift
@@ -10,5 +10,29 @@ import Foundation
 import UIKit
 
 class cashDelegate: NSObject,UITextFieldDelegate {
+     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        let digitString = newString
+            .replacingOccurrences(of:"$", with: "")
+            .replacingOccurrences(of:".", with: "")
+        
+        if let pennies = Int(digitString) {
+            let remainder = pennies % 100
+            let centString = (remainder < 10) ? "0\(remainder)" : "\(remainder)"
+            textField.text = "$\(pennies / 100).\(centString)"
+        }
+        else {
+            textField.text = "$0.00"
+        }
+        return false
+    }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text!.isEmpty {
+            textField.text = "$0.00"
+        }
+    }
+    
+   
 }
